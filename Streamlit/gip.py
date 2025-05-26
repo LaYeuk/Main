d5270e9b5f386c37b36a377f842f0879db01668e
LaYeuk <julien.baechler@gipsa.ch>
2025-05-26 16:09:31 +0200

    Update gip.py

--- Streamlit/gip.py
+++ Streamlit/gip.py
@@ -13,7 +13,7 @@ with st.sidebar:
 
 
 # Titre principal
-st.title("Analyse de la police d'assurance blalba")
+st.title("Analyse de la police d'assurance ")
 
 # Section : Entrée des données techniques avec mise en forme des taux en %
 st.sidebar.header("Données techniques")
